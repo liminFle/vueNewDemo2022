37d735b4585ba088385bb5adbb081b22bd7bdb91
limin_yw <huang-l@aeonpeople.biz>
2022-07-20 10:01:11 +0800

    feat: 修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js Applimin" />
+    <HelloWorld msg="Welcome to Your Vue.js App limin " />
   </div>
 </template>
 
